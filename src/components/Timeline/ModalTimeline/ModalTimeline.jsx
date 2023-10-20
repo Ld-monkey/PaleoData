966a878db4ce9e82a3d34e391f6d14a486de4f1a
Chrstn67 <chrstn.hmbrt67@outlook.com>
2023-10-20 16:23:49 +0200

    modification de la frise chronologique + nouvelle modale

--- src/components/Timeline/ModalTimeline/ModalTimeline.jsx
+++ src/components/Timeline/ModalTimeline/ModalTimeline.jsx
@@ -1,18 +1,33 @@
+// ModalTimeline.jsx
+
+import React from 'react';
+import PropTypes from 'prop-types';
 import './ModalTimeline.scss';
 
 const ModalTimeline = ({ isOpen, closeModal, content }) => {
+  if (!isOpen || !content) {
+    return null;
+  }
+
   return (
-    isOpen && (
-      <div className="modal-overlay" onClick={closeModal}>
-        <button className="close-btn" onClick={closeModal}>
-          &times;
-        </button>
-        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+    <div className={`modal-overlay ${isOpen ? 'open' : ''}`} onClick={closeModal}>
+      <button type="button" onClick={closeModal} className="close-btn">
+        X
+      </button>
+      <div className={`modal-content ${isOpen ? 'open' : ''}`} onClick={(e) => e.stopPropagation()}>
+        {/* Utilisez white-space: pre-line pour gérer les sauts de ligne */}
+        <div className="text-content" style={{ whiteSpace: 'pre-line' }}>
           {content}
         </div>
       </div>
-    )
+    </div>
   );
+};
+
+ModalTimeline.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  closeModal: PropTypes.func.isRequired,
+  content: PropTypes.string,
 };
 
 export default ModalTimeline;
