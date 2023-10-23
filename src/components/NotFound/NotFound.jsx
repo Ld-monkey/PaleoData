a006d1c50817643628855b8bf8d49560c476db86
Chrstn67 <chrstn.hmbrt67@outlook.com>
2023-10-23 17:50:12 +0200

    correction import page 404

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -2,7 +2,7 @@ import './NotFound.scss';
 const NotFound = () => {
   return (
     <div className="NotFound">
-      <img src="assets/404-2.png" alt="Page inexistante représentée par un dinosaure dans le désert" />
+      <img src="./assets/404-2.png" alt="Page inexistante représentée par un dinosaure dans le désert" />
     </div>
   );
 };
